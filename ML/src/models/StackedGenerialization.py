4e2dafabf6c546cb9c49fdc0a699b24cd74360a3
Lapis Panda <Jeremy.micheal.McPaul@gmail.com>
2026-03-31 10:53:00 -0400

    building what I know I can build without knowledge of how to connect it to the rest of our code base

--- ML/src/models/StackedGenerialization.py
+++ ML/src/models/StackedGenerialization.py
@@ -3,18 +3,46 @@
 
 
 # load models from file
-def load_all_models(n_models):
+from fileinput import filename
+
+
+def load_all_models():
 	all_models = list()
     # load each model and add to list.  due to the small amount of models, we can just hard code the filenames.  
 	
 	all_models.append()
-	print('>loaded %s' % 
+		print('>loaded %s' % filename)
 	return all_models
 
 # load all models
-n_members = 5
-members = load_all_models(n_members)
+
+members = load_all_models()
 print('Loaded %d models' % len(members))
 
 # need a way to reference the test set.  which means I need knowledge of how the output of the level 0 models is stored, and in what format.  
-# 
+# need to collect the data to feed into the level 1 model. this is the output of the level 0 models on the test set.
+
+
+# fit a model based on the outputs from the ensemble members
+def fit_stacked_model(members, inputX, inputy):
+	# create dataset using ensemble
+	stackedX = stacked_dataset(members, inputX)
+	# fit standalone model
+	model = LogisticRegression()
+	model.fit(stackedX, inputy)
+	return model
+
+# fit stacked model using the ensemble
+model = fit_stacked_model(members, testX, testy)
+
+# make a prediction with the stacked model
+def stacked_prediction(members, model, inputX):
+	# create dataset using ensemble
+	stackedX = stacked_dataset(members, inputX)
+	# make a prediction
+	yhat = model.predict(stackedX)
+	return yhat
+
+#this is using a linear regression model as the level 1 model. with some missing pieces.  
+# but we could use something like a neural network to learn the relationships between the level 0 model outputs and the target variable.  
+# this is a powerful approach, but it also requires more data to train the level 1 model effectively.
